c0a2e640624a807456eef4c2e010cd7e990f7acc
Bodhisha Thomas <bodhisha@gmail.com>
2019-09-04 15:29:34 -0400

    Create ComparreTheTriplets.c

---
+++ ComparreTheTriplets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main()
+{
+    int a[100],b[100],i,ca=0,cb=0;
+    for (i=0;i<3;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for (i=0;i<3;i++)
+    {
+        scanf("%d",&b[i]);
+    }
+
+    for(i=0;i<3;i++)
+    {
+        if(a[i]>b[i])
+        {
+            ca++;
+        }
+        else if(b[i]>a[i])
+        {
+            cb++;
+        }
+    }
+    printf("%d %d",ca,cb);
+    return 0;
+}
