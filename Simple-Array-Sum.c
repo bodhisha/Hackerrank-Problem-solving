5e11ef610dfdc58268077841993ff3575794ca4d
Bodhisha Thomas <bodhisha@gmail.com>
2019-09-04 13:07:34 -0400

    simple array sum

---
+++ Simple-Array-Sum.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int n,i,a[1000],sum=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d ",&a[i]);
+        sum=sum+a[i];
+    }
+    printf("%d",sum);
+}
