df31f6a2e05ed6ad853552fd5d6c24a4118802f2
Bodhisha Thomas <bodhisha@gmail.com>
2019-09-08 14:15:52 -0400

    Cat and a Mouse

---
+++ Cat-and-a-Mouse.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+    int q,i,x[100],y[100],z[100];
+    scanf("%d",&q);
+    for(i=0;i<q;i++)
+    {
+        scanf("%d %d %d", &x[i],&y[i],&z[i]);
+    
+    if(abs(z[i]-x[i])>abs(z[i]-y[i]))
+    {
+        printf("Cat B\n");
+    }
+    else if (abs(z[i]-x[i])<abs(z[i]-y[i]))
+        printf("Cat A\n");
+    else
+        printf("Mouse C\n");
+    }
+}
