5b1c4d0bf5a7852b6f5088bf63d4713ca49a9a4d
Bodhisha Thomas <bodhisha@gmail.com>
2019-09-09 14:00:58 -0400

    Create Diagonal-Difference.c

---
+++ Diagonal-Difference.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+    int i,j,n,a[100][100],dl=0,dr=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
+
+    for(i=0;i<n;i++)
+    {
+        
+        
+        dl=dl+a[i][i];
+        dr=dr+a[i][n-1-i];
+        
+    }
+    printf("%d",abs(dr-dl));
+}
